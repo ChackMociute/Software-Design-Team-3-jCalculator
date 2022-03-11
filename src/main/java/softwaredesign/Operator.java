505a34fd544f580826e6d020bd792b86d6f36a74
ils510 <i.lewis@student.vu.nl>
2022-03-11 09:19:11 +0100

    Made operator a binary tree

--- src/main/java/softwaredesign/Operator.java
+++ src/main/java/softwaredesign/Operator.java
@@ -1,14 +1,15 @@
 package main.java.softwaredesign;
 
+import java.util.ArrayList;
+
 public class Operator extends ASTNode {
-    public ASTNode[] children;
+    public ASTNode left;
+    public ASTNode right;
     public String operator;
 
     public boolean isCalculatable(){
         // If any of the children are uncalculated operators, this node is not calculatable
-        for(ASTNode child : children) if(child instanceof Operator) return false;
-
-        return true;
+        return left instanceof Literal && right instanceof Literal;
     }
 
     public Operator(String operator){
