2b7e69e6f2813379ce75c998f5846688ba7ecfaa
il8677 <jiajiaisaac@gmail.com>
2022-03-12 18:50:43 +0100

    Colours

--- app/src/main/java/softwaredesign/CalculatorInterface.java
+++ app/src/main/java/softwaredesign/CalculatorInterface.java
@@ -9,11 +9,13 @@ public class CalculatorInterface {
     Scanner scanner = new Scanner(System.in);
 
     public void renderEquation(Equation equation){
+        System.out.print("\u001B[34m");
         System.out.println(equation);
     }
 
     public String getInput(){
-        System.out.print("Enter equation : ");
+        System.out.print("\u001B[36m");
+        System.out.println("Enter equation : ");
         return scanner.nextLine();
     }
 }
