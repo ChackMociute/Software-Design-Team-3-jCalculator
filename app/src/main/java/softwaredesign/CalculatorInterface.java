a9bb9b03fe25df058a2d41d65873aad9a90648cc
il8677 <jiajiaisaac@gmail.com>
2022-03-12 19:00:11 +0100

    Disabled colours

--- app/src/main/java/softwaredesign/CalculatorInterface.java
+++ app/src/main/java/softwaredesign/CalculatorInterface.java
@@ -9,12 +9,12 @@ public class CalculatorInterface {
     Scanner scanner = new Scanner(System.in);
 
     public void renderEquation(Equation equation){
-        System.out.print("\u001B[34m");
+        //System.out.print("\u001B[34m");
         System.out.println(equation);
     }
 
     public String getInput(){
-        System.out.print("\u001B[36m");
+        //System.out.print("\u001B[36m");
         System.out.println("Enter equation : ");
         return scanner.nextLine();
     }
